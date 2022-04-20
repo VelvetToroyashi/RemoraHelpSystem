4369605b7b2a65eec0fdc9d5ae23fb256b1584a7
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-04-20 01:37:33 -0400

    Add help command

---
+++ VTP.Remora.Commands.HelpSystem/HelpCommand.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel;
+using Microsoft.Extensions.Options;
+using Remora.Commands.Attributes;
+using Remora.Commands.Groups;
+using Remora.Discord.Commands.Contexts;
+using Remora.Results;
+using VTP.Remora.Commands.HelpSystem.Services;
+
+namespace VTP.Remora.Commands.HelpSystem;
+
+public class HelpCommand : CommandGroup
+{
+    private readonly ICommandContext _context;
+    private readonly ICommandHelpService _commandHelp; 
+    private readonly IOptions<HelpSystemOptions> _options;
+    
+    
+    public HelpCommand(ICommandContext context, ICommandHelpService commandHelp, IOptions<HelpSystemOptions> options)
+    {
+        _context     = context;
+        _commandHelp = commandHelp;
+        _options     = options;
+    }
+
+    [Command("help")]
+    [Description("Displays help for a command or group of commands.")]
+    public Task<Result> ShowHelpAsync([Greedy] string? command = null)
+        => _commandHelp.ShowHelpAsync(_context.ChannelID, command, _options.Value.TreeName);
+
+}
