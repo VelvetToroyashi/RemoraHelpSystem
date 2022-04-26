b6e26e7b3c51645086069b31716d7458a078e21b
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-04-26 01:06:07 -0400

    class because my tests borked

--- VTP.Remora.Commands.HelpSystem/HelpSystemOptions.cs
+++ VTP.Remora.Commands.HelpSystem/HelpSystemOptions.cs
@@ -5,7 +5,16 @@ namespace VTP.Remora.Commands.HelpSystem;
 /// <summary>
 /// Options related to the command help system.
 /// </summary>
-/// <param name="TreeName">The tree to search when looking for commands.</param>
-/// <param name="DisplayMode">Whether or not to show commands regardless of if their conditions have been met.</param>
 [ExcludeFromCodeCoverage]
-public record HelpSystemOptions(string? TreeName, bool AlwaysShowCommands = false);
+public class HelpSystemOptions
+{
+    /// <summary>
+    /// The tree to search when looking for commands.
+    /// </summary>
+    public string? TreeName { get; set; }
+    
+    /// <summary>
+    /// Whether or not to show commands regardless of if their conditions have been met.
+    /// </summary>
+    private bool AlwaysShowCommands { get; set; }
+}
