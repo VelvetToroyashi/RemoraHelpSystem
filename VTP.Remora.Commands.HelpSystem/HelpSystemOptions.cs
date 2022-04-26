1b26a8e5d0377cdf1725eb6c17ee3d6942457f97
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-04-25 20:20:43 -0400

    Add HelpConditionDisplayMode

--- VTP.Remora.Commands.HelpSystem/HelpSystemOptions.cs
+++ VTP.Remora.Commands.HelpSystem/HelpSystemOptions.cs
@@ -6,5 +6,6 @@ namespace VTP.Remora.Commands.HelpSystem;
 /// Options related to the command help system.
 /// </summary>
 /// <param name="TreeName">The tree to search when looking for commands.</param>
+/// <param name="DisplayMode">See <see cref="HelpConditionDisplayMode"/>; defaults to <see cref="HelpConditionDisplayMode.HideAll"/></param>
 [ExcludeFromCodeCoverage]
-public record HelpSystemOptions(string? TreeName);
+public record HelpSystemOptions(string? TreeName, HelpConditionDisplayMode DisplayMode = HelpConditionDisplayMode.HideAll);
