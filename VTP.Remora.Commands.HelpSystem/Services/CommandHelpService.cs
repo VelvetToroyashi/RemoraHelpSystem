11b9a6aa568bfe3f2ad601d961c58b992e25daac
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-04-28 03:44:54 -0400

    Remove nodes if a condition fails

--- VTP.Remora.Commands.HelpSystem/Services/CommandHelpService.cs
+++ VTP.Remora.Commands.HelpSystem/Services/CommandHelpService.cs
@@ -123,6 +123,7 @@ public class CommandHelpService : ICommandHelpService
                     }
                     else
                     {
+                        successfulNodes.Remove(node);
                         break;
                     }
                 }
