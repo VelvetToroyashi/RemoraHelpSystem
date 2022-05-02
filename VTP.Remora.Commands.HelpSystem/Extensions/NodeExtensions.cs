d1aa59c76fe8dffc7cbc8afa8f3dce97ec711a94
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-05-01 21:35:13 -0400

    Extension method

--- VTP.Remora.Commands.HelpSystem/Extensions/NodeExtensions.cs
+++ VTP.Remora.Commands.HelpSystem/Extensions/NodeExtensions.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 using Remora.Commands;
 using Remora.Commands.Trees.Nodes;
 
@@ -32,5 +33,37 @@ public static class GroupNodeExtensions
             return null;
         
         return string.IsNullOrEmpty(cn.Shape.Description) ? null : cn.Shape.Description;
+    }
+
+    /// <summary>
+    /// Attempts to gather an attribute from a given <see cref="IChildNode"/>, traversing only it's direct anscestors.
+    /// </summary>
+    /// <param name="startingNode">The node to start traversing from.</param>
+    /// <typeparam name="TAttribute">The attribute to find on the tree.</typeparam>
+    /// <returns>The found attribute if any, otherwise null.</returns>
+    public static TAttribute? GetAttributeFromTree<TAttribute>(this IChildNode startingNode) where TAttribute : Attribute
+    {
+        IChildNode? parent = null;
+        TAttribute? returnAttribute = null;
+        
+        
+        if (startingNode is CommandNode cn)
+        {
+            if (cn.CommandMethod.GetCustomAttribute<TAttribute>() is { } commandAttribute)
+                return commandAttribute;
+        }
+        
+        do
+        {
+            parent = (IChildNode)(parent?.Parent ?? startingNode.Parent);
+
+            var attributes = ((GroupNode)parent).GroupTypes.SelectMany(gt => gt.GetCustomAttributes<TAttribute>());
+            
+            if (attributes.FirstOrDefault() is {} attribute)
+                returnAttribute = attribute;
+        }
+        while (parent.Parent is GroupNode && returnAttribute is null);
+        
+        return returnAttribute;
     }
 }
